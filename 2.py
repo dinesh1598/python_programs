85491cbd0adb96db5a5331d413f78beac30fdb47
dineshThanigaivel <101261835+dinesh1598@users.noreply.github.com>
2022-04-07 13:16:16 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,15 @@
+#Return the Sum of Two Numbers
+#method 1
+"""def ad(a,b):
+   return a+b
+print(ad(23,6))
+#approach 2
+a=int(input("Enter the 1st input:"))
+b=int(input("Enter the 2nd input:"))
+sum=a+b
+print(sum)
+"""
+a=int(input("Enter the number:"))
+b=int(input("Enter the numbner 2:"))
+sum=a+b
+print("The sum is:",sum)
