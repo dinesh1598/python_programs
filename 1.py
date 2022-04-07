93adc9975239744577585fe0b9ae2e0c02cd15f5
dineshThanigaivel <101261835+dinesh1598@users.noreply.github.com>
2022-04-07 12:54:38 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,11 @@
+#method 1
+def hours_2_seconds(hours):
+    sec= hours * 3600
+    return sec
+convert_hours=int(input("(Method1)Enter the number to convert:"))
+print(hours_2_seconds(convert_hours))
+
+#method 2
+hour=int(input("(method2)Enter the number:"))
+seconds = hour * 3600
+print(seconds)
