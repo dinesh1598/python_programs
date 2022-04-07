e4f8038d197f27659d2b1af44259eb2456db7c9b
dineshThanigaivel <101261835+dinesh1598@users.noreply.github.com>
2022-04-07 13:30:55 +0530

    Add files via upload

--- 25.py
+++ 25.py
@@ -3,7 +3,9 @@ Create a recursive function that takes two parameters and repeats the string n n
 The first parameter text is the string to be repeated and the second parameter is the number of times 
 the string is to be repeated.
 """
-"""#method 1 
+
+#method 1 
+
 def Recur(a,b):
    i=0
    while i<b:
@@ -11,9 +13,13 @@ def Recur(a,b):
        i=i+1
 str_repeat=input("Enter the string:")
 time_repeat=int(input("Enter the number to repeat:"))
-Recur(str_repeat,time_repeat)"""
+Recur(str_repeat,time_repeat)
+
+#method 2
+
 def using_operator(strng,n):
     return strng * n
+#by using the star operator
 
 time_repeat=int(input("Enter the number to repeat:"))
 print(using_operator("sethu\n",time_repeat))
