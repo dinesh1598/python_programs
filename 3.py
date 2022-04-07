2755ff892fce8c92bb46f291e891feded5a4d40f
dineshThanigaivel <101261835+dinesh1598@users.noreply.github.com>
2022-04-07 13:28:13 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,14 @@
+#Return the Next Number from the Integer Passed
+
+#method 1
+
+i=int(input("(method1)Enter the number:"))
+print("(method1)The next integer is:",i+1)
+
+
+#method 2
+def next_int(value):
+    return value+1
+    
+user_input=int(input("(method 2)Enter the number to print next number:"))    
+print(next_int(user_input))
